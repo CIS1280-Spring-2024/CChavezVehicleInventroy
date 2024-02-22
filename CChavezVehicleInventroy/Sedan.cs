b1af88bf83515204be55b451a377f54ee50baa8e
HitandFeyd <cchavez572@cnm.edu>
2024-02-22 15:22:44 -0700

    Add project files.

---
+++ CChavezVehicleInventroy/Sedan.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CChavezVehicleInventory
+{
+    //  4.1.	Make a sedan class that inherits from the Vehicle class:
+    //class Sedan : Vehicle
+    //{
+
+    //}
+
+    //  4.2.	Notice that Sedan has a red squiggle.Hovering over it notice that visual studio tells us we must implement GetDescription, Wheels property and MaxPassengers property!
+    //  4.3.	Add the following code:
+    public class Sedan : Vehicle
+    {
+        private int wheels = 4;
+        public override int Wheels
+        {
+            get { return wheels; }
+        }
+
+        public override int MaxPassengers
+        {
+            get
+            {
+                return 5;
+            }
+        }
+
+        public override string GetDescription()
+        {
+            return "Sedan License Plate: " + LicensePlate +
+                " VIN " + VIN + " Wheels: " + wheels + " Passengers: " + MaxPassengers;
+        }
+        //  4.4.	Let’s also override ToString:
+        public override string ToString()
+        {
+            return "Sedan License Plate: " + LicensePlate +
+                " VIN " + VIN;
+        }
+
+    }
+
+}
---
+++ CChavezVehicleInventroy/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CChavezVehicleInventory
+{
+    //  3.1.	Create an abstract class that represents the common attributes of our vehicles:
+    public abstract class Vehicle
+    {
+        public string VIN { get; set; }
+
+        public string LicensePlate { get; set; }
+
+        public abstract int Wheels { get; }
+
+        public abstract int MaxPassengers { get; }
+
+        public abstract string GetDescription();
+
+        //  3.2.	Notice that some of our properties/methods are concrete and some are declared abstract.
+    }
+
+}
